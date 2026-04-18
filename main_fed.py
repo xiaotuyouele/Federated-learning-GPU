166bf0dc2691f81bfeef32994c04a9310b765c75
xiaotuyouele <hajwctbsusytsjisct@gmail.com>
2026-04-18 17:51:42 +0800

    Update main_fed.py

--- main_fed.py
+++ main_fed.py
@@ -41,10 +41,10 @@ def generate_synthetic_dataset(args, seed=0):
     num_classes = args.num_classes
 
     # 类中心更近一些
-    centers = torch.randn(num_classes, input_size) * 0.8
+    centers = torch.randn(num_classes, input_size) * 0.5
 
     labels = torch.randint(0, num_classes, (num_samples,))
-    features = centers[labels] + 1.5 * torch.randn(num_samples, input_size)
+    features = centers[labels] + 2.0 * torch.randn(num_samples, input_size)
     noise_ratio = 0.10
     noisy_mask = torch.rand(num_samples) < noise_ratio
     noisy_labels = torch.randint(0, num_classes, (num_samples,))

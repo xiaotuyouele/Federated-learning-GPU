17dabc67e6d5234ce3e2a6ee33f048ea9ed93623
xiaotuyouele <hajwctbsusytsjisct@gmail.com>
2026-04-21 11:50:25 +0800

    Update Fed.py

--- models/Fed.py
+++ models/Fed.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Python version: 3.6
 
 import copy
 import torch
-from torch import nn
-
 
 def FedAvg(w):
+    if len(w) == 0:
+        raise ValueError("FedAvg received empty weights list")
+
     w_avg = copy.deepcopy(w[0])
+
     for k in w_avg.keys():
-        for i in range(1, len(w)):
-            w_avg[k] += w[i][k]
-        w_avg[k] = torch.div(w_avg[k], len(w))
+        stacked = torch.stack([client_w[k] for client_w in w], dim=0)
+        w_avg[k] = torch.mean(stacked, dim=0)
+
     return w_avg
